5f3e3980e1d705c893629fcb100715b4ce88188d
Renato Brito <rb.renatobrito@gmail.com>
2020-08-22 06:08:41 -0300

    Criado end-point para deletar um produto.

--- DeletarProduto/index.js
+++ DeletarProduto/index.js
@@ -1,16 +1,18 @@
+const { ObjectID } = require("mongodb");
 const createMongoClient = require("../shared/mongoClient");
 
 module.exports = async function (context, req) {
+    const { id } = req.params;
+
     const { client: MongoClient, closeConnectionFn } = await createMongoClient();
 
     const Produtos = MongoClient.collection("produtos");
-    const res = await Produtos.find({});
-    const body = await res.toArray();
+    const res = await Produtos.findOneAndDelete({ _id: ObjectID(id) });
 
     closeConnectionFn();
 
     context.res = {
         status: 200,
-        body: body
+        body: res
     };
-};
+}
